56eaf8d6edfd91d676d5c2503dd51e7eaa287d26
Benjamin <benjamin.defant@tum.de>
2023-03-03 19:53:01 +0100

    .

--- deep_dive.py
+++ deep_dive.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import analyze_lib as al
+import os
 
 
 def deep_dive(product_key, transactions):
+    if not os.path.exists(f'deep_dive_{product_key}'):
+        os.mkdir(f'deep_dive_{product_key}')
+
     product_transactions = transactions[transactions['ProductKey'] == product_key].compute()
     product_values = product_transactions.iloc[0, [13, 14, 15]]
     brand_key, supplier_key, product_category_lvl2 = product_values
@@ -11,17 +15,18 @@ def deep_dive(product_key, transactions):
     print(f'Deep dive for ProductKey {product_key}:')
     print(product_values)
 
-
     fig, ax = plt.subplots()
     weekly_sales = al.sum_values_groupby(product_transactions, 'Week', 'ActualSales')
     weekly_sales.reset_index().plot(x='Week', y='ActualSales', title='Product sales vs. category sales', ax=ax)
     al.mean_values_groupby(al.sum_values_groupby_c(other_products, ['Week', 'ProductKey'], 'ActualSales').reset_index(), 'Week', 'ActualSales')\
         .reset_index().plot(x='Week', y='ActualSales', ax=ax)
     plt.legend([f'ProductKey {product_key}', f'Avg ProductCategory {product_category_lvl2}'])
+    plt.savefig(f'deep_dive_{product_key}/sales_vs_category_{product_key}.png')
 
     (weekly_sales / al.sum_values_groupby_c(other_products, 'Week', 'ActualSales'))\
         .reset_index().plot(x='Week', y='ActualSales', title=f'Sales percentage in product category {product_category_lvl2}')
     plt.legend([f'ProductKey {product_key}'])
+    plt.savefig(f'deep_dive_{product_key}/sales_percentage_{product_key}.png')
 
     fig, ax = plt.subplots()
     weekly_volume = al.sum_values_groupby(product_transactions, 'Week', 'UnitVolume')
@@ -29,11 +34,13 @@ def deep_dive(product_key, transactions):
     al.mean_values_groupby(al.sum_values_groupby_c(other_products, ['Week', 'ProductKey'], 'UnitVolume').reset_index(), 'Week', 'UnitVolume') \
         .reset_index().plot(x='Week', y='UnitVolume', ax=ax)
     plt.legend([f'ProductKey {product_key}', f'Avg ProductCategory {product_category_lvl2}'])
+    plt.savefig(f'deep_dive_{product_key}/volume_vs_category_{product_key}.png')
 
     (weekly_volume / al.sum_values_groupby_c(other_products, 'Week', 'UnitVolume')) \
         .reset_index().plot(x='Week', y='UnitVolume',
                             title=f'UnitVolume percentage in product category {product_category_lvl2}')
     plt.legend([f'ProductKey {product_key}'])
+    plt.savefig(f'deep_dive_{product_key}/volume_percentage_{product_key}.png')
 
 
 
@@ -44,5 +51,3 @@ deep_dive(49340, all_transactions)
 deep_dive(49341, all_transactions)
 deep_dive(49333, all_transactions)
 deep_dive(49329, all_transactions)
-
-plt.show()
--- product_performance.py
+++ product_performance.py
@@ -11,7 +11,7 @@ holidays = holidays['Date'].values.compute()
 
 al.split_dates_holidays(all_transactions, holidays)
 
-
+"""
 al.sum_values_groupby_to_csv(all_transactions, 'Region_Lvl1', 'ActualSales', 'performance_stats/region_lvl1.csv')
 
 al.sum_values_groupby_to_csv(all_transactions, 'Region_Lvl2', 'ActualSales', 'performance_stats/region_lvl2.csv')
@@ -35,31 +35,56 @@ al.sum_values_groupby_to_csv(all_transactions[all_transactions['ProductCategory_
 
 al.sum_values_groupby_to_csv(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AE'], 'ProductKey',
                              'ActualSales', 'performance_stats/product_category_lvl2_ae.csv')
-
-
+"""
+"""
 al.sum_values_groupby_c(all_transactions, 'Week', 'ActualSales').reset_index().plot(x='Week', y='ActualSales')
+plt.savefig('plots/weekly_sales.png')
 
 al.sum_values_groupby_c(all_transactions, 'Month', 'ActualSales').reset_index().plot(x='Month', y='ActualSales')
+plt.savefig('plots/monthly_sales.png')
 
 al.sum_values_groupby_c(all_transactions, 'Quarter', 'ActualSales').reset_index().plot(x='Quarter', y='ActualSales')
+plt.savefig('plots/quarterly_sales.png')
 
 al.sum_values_groupby_c(all_transactions, ['Week', 'Region_Lvl1'], 'ActualSales').unstack().plot()
-
+plt.savefig('plots/regional_sales.png')
+"""
 al.sum_values_groupby_c(all_transactions, ['Week', 'ProductCategory_Lvl2'], 'ActualSales').unstack().plot()
+plt.savefig('plots/category_sales.png')
 
+al.sum_values_groupby_c(all_transactions, ['Week', 'ProductCategory_Lvl2'], 'ActualSales').unstack().plot(kind='area')
+plt.savefig('plots/category_sales2.png')
+"""
 al.sum_values_groupby_c(all_transactions, ['Week', 'ProductCategory_Lvl2'], 'UnitVolume').unstack().plot()
+plt.savefig('plots/volume_sales.png')
 
-al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AD'], ['Week', 'ProductCategory_Lvl2'], 'UnitVolume').unstack().plot()
+al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AA'], ['Week', 'ProductKey'], 'ActualSales').unstack().plot(kind='area', legend=False)
+plt.savefig('plots/aa_sales.png')
 
-al.sum_values_groupby_c(all_transactions[all_transactions['BrandKey'].isin([3521, 1713, 2435, 2146])], ['Week', 'BrandKey'], 'UnitVolume').unstack().plot()
+al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AB'], ['Week', 'ProductKey'], 'ActualSales').unstack().plot(kind='area', legend=False)
+plt.savefig('plots/ab_sales.png')
+
+al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AC'], ['Week', 'ProductKey'], 'ActualSales').unstack().plot(kind='area', legend=False)
+plt.savefig('plots/ac_sales.png')
+
+al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AD'], ['Week', 'ProductKey'], 'ActualSales').unstack().plot(kind='area', legend=False)
+plt.savefig('plots/ad_sales.png')
+
+al.sum_values_groupby_c(all_transactions[all_transactions['ProductCategory_Lvl2'] == 'AE'], ['Week', 'ProductKey'], 'ActualSales').unstack().plot(kind='area', legend=False)
+plt.savefig('plots/ae_sales.png')
+
+al.sum_values_groupby_c(all_transactions[all_transactions['BrandKey'].isin([3521, 1713, 2435, 2146])], ['Week', 'BrandKey'], 'ActualSales').unstack().plot()
+plt.savefig('plots/brand_sales.png')
 
 al.mean_values_groupby(al.sum_values_groupby_c(all_transactions, ['TransactionDate', 'Week', 'WeekendFlag'], 'ActualSales').reset_index(),
                        ['Week', 'WeekendFlag'], 'ActualSales').unstack().plot()
+plt.savefig('plots/workday_weekend_avg_sales.png')
 
 fig, ax = plt.subplots()
 al.sum_values_groupby_c(all_transactions, 'TransactionDate', 'ActualSales').reset_index().plot(x='TransactionDate', y='ActualSales', ax=ax)
 for date in holidays:
     ax.axvline(x=date, linestyle='dashed', alpha=0.5)
+plt.savefig('plots/holiday_sales.png')
 
 fig, ax = plt.subplots()
 
@@ -72,7 +97,7 @@ monthly_cpi['Avg_change'] = monthly_cpi['CPI_monthly'].diff() / (monthly_cpi['CP
 monthly_increase = monthly_cpi.plot(x='Date_monthly', y='Avg_change', ax=ax)
 del monthly_sales_increase
 del monthly_increase
+plt.savefig('plots/sales_increase_cpi.png')
 
-print(al.mean_values_groupby(al.sum_values_groupby_c(all_transactions, ['TransactionDate', 'IsHoliday'], 'ActualSales').reset_index(), 'IsHoliday'))
-
-plt.show()
+print(al.mean_values_groupby(al.sum_values_groupby_c(all_transactions, ['TransactionDate', 'IsHoliday'], 'ActualSales').reset_index(), 'IsHoliday', 'ActualSales'))
+"""
